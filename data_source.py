e8a8a5e32ac942b81d3ddcbfe20a71470bb114d7
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2019-05-15 18:03:40 -0500

    Add base files for classes

---
+++ data_source.py
@@ -0,0 +1,8 @@
+# The real historical data.
+class RealStockData:
+    pass
+
+# Data that has the shape of sinusoid.
+# Should work good as a proof of concept.
+class SinusoidStockData:
+    pass
---
+++ training_session.py
@@ -0,0 +1,2 @@
+class TrainingSession:
+    pass
