8712386fbfffcfcd3b28139f589b5ee9b4251fff
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2019-05-16 00:05:54 -0500

    Moving stuff around, JSON fix

---
+++ json/json_writer.py
@@ -0,0 +1,23 @@
+import json
+
+# TODO(iryzhkov): Finish the dictionary with all the possible
+#                 json objects
+json_dictionary = {
+        'date_range': None,
+
+        'session': None,
+
+        'ml_agent': None,
+        'agent_configuration': None,
+
+        'data_source': None,
+        'real_data': None,
+        'sinusoid_data': None,
+        'random_data': None,
+}
+
+def getObjectFromFile(filename):
+    # TODO(iryzhkov): Check if the file is real and json.
+    #                 Get dict from file, from the dict
+    #                 initialize the correct object.
+    pass
